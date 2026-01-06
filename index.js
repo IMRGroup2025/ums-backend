a551ecf9a877865fbe795ee0722d28da884948c1
Senumi Costa <costasenumi05@gmail.com>
2026-01-07 02:10:19 +0530

    Refactor code structure for improved readability and maintainability

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ import express from "express";
 import cors from "cors";
 import customerRoutes from "./routes/customer.routes.js";
 import meterRoutes from "./routes/meter.routes.js"
+import billRoutes from "./routes/bill.routes.js"
+import dashboardRoutes from "./routes/dashboard.routes.js"
+import exportRoutes from "./routes/export.routes.js"
+import paymentRoutes from "./routes/payment.routes.js"
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use("/api/bills", billRoutes);
+app.use("/api/dashboard", dashboardRoutes)
+app.use("/api/export", exportRoutes)
+app.use("/api/payments", paymentRoutes)
 
 // Root test route
 app.get("/", (req, res) => {
