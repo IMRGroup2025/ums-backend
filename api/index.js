547927c6fa5e2300ca509d15d81a09497a950960
Senumi Costa <costasenumi05@gmail.com>
2026-01-11 21:07:53 +0530

    feat: create initial API structure with routing and server setup

--- api/index.js
+++ api/index.js
@@ -1,51 +1,41 @@
 import express from "express";
 import cors from "cors";
+
 import customerRoutes from "./routes/customer.routes.js";
-import meterRoutes from "./routes/meter.routes.js"
-import meterReadingRoutes from "./routes/meterReading.routes.js"
-import billRoutes from "./routes/bill.routes.js"
-import dashboardRoutes from "./routes/dashboard.routes.js"
-import exportRoutes from "./routes/export.routes.js"
+import meterRoutes from "./routes/meter.routes.js";
+import meterReadingRoutes from "./routes/meterReading.routes.js";
+import billRoutes from "./routes/bill.routes.js";
+import dashboardRoutes from "./routes/dashboard.routes.js";
+import exportRoutes from "./routes/export.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import tariffPlansRoutes from "./routes/tariffPlans.routes.js";
 import complaintRoutes from "./routes/complaints.routes.js";
-import paymentRoutes from "./routes/payment.routes.js"; 
+import paymentRoutes from "./routes/payment.routes.js";
 import utilityRoutes from "./routes/utility.routes.js";
-import { authenticate } from "./middlewares/authenticate.middleware.js";
-import { authorize } from "./middlewares/authorize.middleware.js";
-
 
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/bills", billRoutes);
-app.use("/api/dashboard", dashboardRoutes)
-app.use("/api/export", exportRoutes)
+app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/export", exportRoutes);
 app.use("/api/auth", authRoutes);
-app.use("/api/meter-readings", meterReadingRoutes)
-app.use("/api/meters", meterRoutes)
-app.use("/api/customers", customerRoutes);  
+app.use("/api/meter-readings", meterReadingRoutes);
+app.use("/api/meters", meterRoutes);
+app.use("/api/customers", customerRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/tariff-plans", tariffPlansRoutes);
 app.use("/api/complaints", complaintRoutes);
 app.use("/api/utilities", utilityRoutes);
 app.use("/api/payments", paymentRoutes);
 
-
-
-
-
 app.get("/", (req, res) => {
   res.send("Utility Management System API is running");
 });
 
 
-
-
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+ 
+export default app;
