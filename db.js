3878caa9471111738e6143e4678a0c2a272f03c8
Senumi Costa <costasenumi05@gmail.com>
2026-01-12 11:11:51 +0530

    Revert "Prepare backend for deployment (ignore node_modules, env setup)"
    This reverts commit 443e56a5d1541c5aaffda3d03214bd5b2ab50a6a.
    
     On branch main
    	modified:   db.js

--- db.js
+++ db.js
@@ -1,14 +1,13 @@
-import mysql from "mysql2/promise";
+import mysql from "mysql2";
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: "localhost",
+  user: "root",
+  password: "senumi@2005",
+  database: "ums_dbase",
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
 });
 
 export default db;
+
